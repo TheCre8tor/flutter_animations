7a3d2359f46fd5946163edeca2f90f147cdf75e6
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-10-09 18:16:33 +0100

    completed lecture 4

--- chapter_3/lib/main.dart
+++ chapter_3/lib/main.dart
@@ -1,11 +1,14 @@
+import 'dart:math' show pi;
+
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +19,164 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: const Home(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+const width = 100.0;
+const height = 100.0;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  late AnimationController _xController;
+  late AnimationController _yController;
+  late AnimationController _zController;
+
+  late Tween<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _xController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+
+    _yController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 30),
+    );
+
+    _zController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 40),
+    );
+
+    _animation = Tween<double>(
+      begin: 0,
+      end: pi * 2, // 360 degree
+    );
+  }
+
+  @override
+  void dispose() {
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    _xController
+      ..reset()
+      ..repeat();
+
+    _yController
+      ..reset()
+      ..repeat();
+
+    _zController
+      ..reset()
+      ..repeat();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: height, width: double.infinity),
+            AnimatedBuilder(
+              animation: Listenable.merge([
+                _xController,
+                _yController,
+                _zController,
+              ]),
+              builder: (context, child) {
+                return Transform(
+                  alignment: Alignment.center, // center point
+                  transform: Matrix4.identity()
+                    ..rotateX(_animation.evaluate(_xController))
+                    ..rotateY(_animation.evaluate(_yController))
+                    ..rotateZ(_animation.evaluate(_zController)),
+                  child: Stack(
+                    children: [
+                      // back side
+                      Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..translate(Vector3(0, 0, -width)),
+                        child: Container(
+                          width: width,
+                          height: height,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      //left side
+                      Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()..rotateY(pi / 2.0),
+                        child: Container(
+                          width: width,
+                          height: height,
+                          color: Colors.red,
+                        ),
+                      ),
+                      //left side
+                      Transform(
+                        alignment: Alignment.centerRight,
+                        transform: Matrix4.identity()..rotateY(-pi / 2.0),
+                        child: Container(
+                          width: width,
+                          height: height,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      // top side
+                      Transform(
+                        alignment: Alignment.topCenter,
+                        transform: Matrix4.identity()..rotateX(-pi / 2.0),
+                        child: Container(
+                          width: width,
+                          height: height,
+                          color: Colors.orange,
+                        ),
+                      ),
+                      // bottom side
+                      Transform(
+                        alignment: Alignment.bottomCenter,
+                        transform: Matrix4.identity()..rotateX(pi / 2.0),
+                        child: Container(
+                          width: width,
+                          height: height,
+                          color: Colors.brown,
+                        ),
+                      ),
+                      // front side
+                      Container(
+                        width: width,
+                        height: height,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
